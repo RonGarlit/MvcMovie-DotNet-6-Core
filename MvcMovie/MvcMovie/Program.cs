b62d4f3ca134372e0521af4d4419a49f0335b79b
rgarlit <rgarlit@fgcu.edu>
2022-06-28 16:15:05 -0400

    Tweaking notes in Program.cs

--- MvcMovie/MvcMovie/Program.cs
+++ MvcMovie/MvcMovie/Program.cs
@@ -8,74 +8,78 @@ using MvcMovie.Models;
 //=====================================================================================
 namespace MvcMovie
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
-			//=====================================================================================
-			// Patch in the connection sting that is in the appsettings.json file
-			//=====================================================================================
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            //=====================================================================================
+            // Patch in the connection sting that is in the appsettings.json file
+            //=====================================================================================
 
-			builder.Services.AddDbContext<MvcMovieContext>(options =>
-				options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
+            builder.Services.AddDbContext<MvcMovieContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
 
-			var app = builder.Build();
+            var app = builder.Build();
 
-			//=====================================================================================
-			// Seed the database
-			//=====================================================================================
-			using (var scope = app.Services.CreateScope())
-			{
-				var services = scope.ServiceProvider;
+            //=====================================================================================
+            // Seed the database
+            //=====================================================================================
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
 
-				SeedData.Initialize(services);
-			}
+                SeedData.Initialize(services);
+            }
 
-			//=====================================================================================
-			// For production environments that are implementing HTTPS for the first time, set the
-			// initial HstsOptions.MaxAge to a small value using one of the TimeSpan methods. Set
-			// the value from hours to no more than a single day in case you need to revert the
-			// HTTPS infrastructure to HTTP. After you're confident in the sustainability of the
-			// HTTPS configuration, increase the HSTS max-age value; a commonly used value is one year.
-			//=====================================================================================
-			//The following highlighted code:
-			//=====================================================================================
-			//builder.Services.AddHsts(options =>
-			//{
-			//    options.Preload = true;
-			//    options.IncludeSubDomains = true;
-			//    options.MaxAge = TimeSpan.FromDays(60);
-			//    options.ExcludedHosts.Add("example.com");
-			//    options.ExcludedHosts.Add("www.example.com");
-			//});
+            //=====================================================================================
+            // For production environments that are implementing HTTPS for the first time, set the
+            // initial HstsOptions.MaxAge to a small value using one of the TimeSpan methods. Set
+            // the value from hours to no more than a single day in case you need to revert the
+            // HTTPS infrastructure to HTTP. After you're confident in the sustainability of the
+            // HTTPS configuration, increase the HSTS max-age value; a commonly used value is one year.
+            //=====================================================================================
+            //The following highlighted code:
+            //=====================================================================================
+            //builder.Services.AddHsts(options =>
+            //{
+            //    options.Preload = true;
+            //    options.IncludeSubDomains = true;
+            //    options.MaxAge = TimeSpan.FromDays(60);
+            //    options.ExcludedHosts.Add("example.com");
+            //    options.ExcludedHosts.Add("www.example.com");
+            //});
 
-			//=====================================================================================
-			// Configure the HTTP request pipeline. ASP.NET Core implements HSTS with the UseHsts
-			// extension method. The following code calls UseHsts when the app isn't in development mode.
-			//=====================================================================================
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            //=====================================================================================
+            // Configure the HTTP request pipeline. ASP.NET Core implements HSTS with the UseHsts
+            // extension method. The following code calls UseHsts when the app isn't in development mode.
+            //=====================================================================================
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
+            //=====================================================================================
+            // When authorizing a resource that is routed using endpoint routing, this call must
+            // appear between the calls to app.UseRouting() and app.UseEndpoints(...) for the
+            // middleware to function correctly.
+            app.UseAuthorization();
+            //=====================================================================================
 
-			app.UseAuthorization();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
-
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
